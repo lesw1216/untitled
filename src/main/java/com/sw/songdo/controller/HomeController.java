fe6b03a2818932d957064f782491500f453a8909
lesw1216 <lesw1216@gmail.com>
2022-08-23 01:54:42 +0900

    카카오API를 이용한 지도 삽입

--- src/main/java/com/sw/songdo/controller/HomeController.java
+++ src/main/java/com/sw/songdo/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.sw.songdo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
+        UserDetails userDetails = (UserDetails) principal;
+        String username = userDetails.getUsername();
+        String password = userDetails.getPassword();
+        log.info("Authentication user[{}][{}]", username, password);
         return "/index";
     }
 }
